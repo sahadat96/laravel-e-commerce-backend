4a0a72a20a26d6fe32717c96b4e1819b8842899a
Sahadat Hossain <128299846+sahadat96@users.noreply.github.com>
2026-05-20 17:41:59 +0600

    Delete resources/js directory

--- resources/js/assets/allcss..js
+++
@@ -1,7 +0,0 @@
-import './css/plugins/slider-range.css';
-import './css/main.css';
-import './css/custom.css';
-import './css/plugins/animate.min.css';
-import './css/plugins/slider-range.css';
-
-import './sass/allsass';
